0481a57f0752352e991a4b151fc4d0e64edf82af
LukaZSH <lukaalvesclaro@gmail.com>
2025-04-04 20:17:42 -0300

    Adiciona definição da porta para compatibilidade com Railway

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__, static_folder="static")
 
@@ -9,3 +10,7 @@ def home():
 @app.route("/<path:path>")
 def static_files(path):
     return send_from_directory("static", path)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
